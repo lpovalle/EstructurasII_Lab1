626f77aea0b3e94971de5dacc70b209afc7a1209
Luis Ovalle <luispalp@hotmail.com>
2018-09-19 22:06:55 -0600

    Creacion de proyecto

---
+++ app/src/main/java/com/ed2/lab1/MainActivity.java
@@ -0,0 +1,56 @@
+package com.ed2.lab1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.nbsp.materialfilepicker.MaterialFilePicker;
+import com.nbsp.materialfilepicker.ui.FilePickerActivity;
+
+import java.io.File;
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnBuscar;
+    TextView lbxRuta;
+    String pathConvertir;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnBuscar = (Button) findViewById(R.id.btn1);
+        lbxRuta = (TextView) findViewById(R.id.lbxMostrarRuta) ;
+
+        btnBuscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                new MaterialFilePicker()
+                        .withActivity(MainActivity.this)
+                        .withRequestCode(1000)
+                        .withHiddenFiles(true) // Show hidden files and folders
+                        .start();
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1000 && resultCode == RESULT_OK) {
+            String filePath = data.getStringExtra(FilePickerActivity.RESULT_FILE_PATH);
+
+            pathConvertir = filePath;
+            lbxRuta.setText(pathConvertir);
+        }
+    }
+}
